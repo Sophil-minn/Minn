2ae97598b060a1924249ab57f854762bd1341885
minmin <1056640663@qq.com>
2022-09-17 21:04:19 +0800

    feat: 徒手写一个button

--- src/button/index.test.tsx
+++ src/button/index.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Button from './index';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Button>click me</Button>);
+  const linkElement = screen.getByText(/click me/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/button/index.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+// （1）传入一个对象：classnames({class1:true,class2:false}) ，
+// true表示相应的class生效，反之false表示不生效。
+//（2）接受多个类名：classnames(class1,class2,{ class3:false })
+import classNames from 'classnames';
+
+import './index.css';
+
+interface buttonProps {
+  className?: string;
+  type?: 'normal' | 'primary';
+  children?: ReactNode;
+}
+
+const Button = (props: buttonProps) => {
+  const { children, className, type = 'normal'} = props;
+  const cls = classNames({
+    'ant-btn': true,
+    [`ant-btn-${type}`]: type,
+    [className as string]: !!className
+  });
+  return <button className={cls}>{children}</button>
+}
+
+export default Button;
