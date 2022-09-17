d92fd24bc77a264be9ee6e85a305921cb263713c
minmin <1056640663@qq.com>
2022-09-17 21:59:22 +0800

    feat： 引入storybook

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import Button from '.';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -19,23 +19,40 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button11',
+  type: 'primary',
+  children: 'Button11',
 };
+
+const style = {
+  marginLeft: '8px'
+}
+
+export const Basic = () => { 
+  return (
+    <>
+    <Button type="primary" >Primary Button</Button>
+    <Button style={style}>Default Button</Button>
+    <Button type="dashed" style={style}>Dashed Button</Button>
+    <br />
+    <Button type="text">Text Button</Button>
+    <Button type="link" style={style}>Link Button</Button>
+  </>
+  )
+}
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  children: 'Button',
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
---
+++ src/stories/index.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react';
+// （1）传入一个对象：classnames({class1:true,class2:false}) ，
+// true表示相应的class生效，反之false表示不生效。
+//（2）接受多个类名：classnames(class1,class2,{ class3:false })
+import classNames from 'classnames';
+
+import './index.css';
+
+interface buttonProps {
+  className?: string;
+  type?: 'normal' | 'primary' | "dashed" | "text" | "link";
+  children?: ReactNode;
+  style?: React.CSSProperties;
+}
+
+const Button = (props: buttonProps) => {
+  const { children, className, type = 'normal', style} = props;
+  const cls = classNames({
+    'ant-btn': true,
+    [`ant-btn-${type}`]: type,
+    [className as string]: !!className
+  });
+  return <button className={cls} style={style}>{children}</button>
+}
+
+export default Button;
