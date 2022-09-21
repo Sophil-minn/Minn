1a9aa75986dba352bc2ebf3bacb10c7a3d01e0f5
minmin <1056640663@qq.com>
2022-09-21 23:08:45 +0800

    feat: radio css

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Icon from './icon/index';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <Icon type="fixed">click me</Icon>
-    <Icon>click me</Icon>
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
