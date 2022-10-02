d3d3b64bfa29af57ee18cfbe04f386d928125394
minmin <1056640663@qq.com>
2022-10-02 20:25:46 +0800

    feat: 受控非受控 单测

--- src/input/index.test.tsx
+++ src/input/index.test.tsx
@@ -3,22 +3,45 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Input from './index';
 
 describe("Input", () => {
-  test('renders Button', () => {
-    render(<Input>click me </Input>)
-    const linkElement = screen.getByText(/click me/i);
-    expect(linkElement).toBeInTheDocument();
-  })
-})
-/**
-test('renders normal Input', () => {
-  const { container } = render(<Input>click me</Input>);
-  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
-  expect(container.querySelector('.ant-btn-normal')).toBeInTheDocument();
-});
+  test('should support defaultValue', () => {
+    const onChange = jest.fn();
+    render(<Input defaultValue="default" data-testid="t1" onChange={onChange} />);
 
-test('renders small Button', () => {
-  const { container } = render(<Input size='small'>click me</Input>);
-  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
-  expect(container.querySelector('.ant-btn-small')).toBeInTheDocument();
-});
-**/
+    const linkElement = screen.getByTestId('t1');
+    expect(linkElement.getAttribute('value')).toBe('default');
+
+    fireEvent.change(linkElement, { target: { value: 'new value' } });
+    expect(onChange).toBeCalledTimes(1);
+    expect(linkElement.getAttribute('value')).toBe('new value');
+  });
+
+  test('should support unchange', () => {
+    const onChange = jest.fn();
+    render(<Input value="abcd" data-testid="t2" onChange={onChange} />);
+
+    const linkElement = screen.getByTestId('t2');
+    expect(linkElement.getAttribute('value')).toBe('abcd');
+
+    fireEvent.change(linkElement, { target: { value: 'new value2' } });
+    expect(onChange).toBeCalledTimes(1);
+    expect(linkElement.getAttribute('value')).toBe('abcd');
+  });
+
+  test('should support under control', () => {
+    const App = () => {
+      const [value, setValue] = React.useState('a3');
+      const handleChange = (e) => {
+        setValue(e.target.value);
+      }
+      return <Input value={value} data-testid="t3" onChange={handleChange} />
+    }
+    render(<App />);
+
+    const linkElement = screen.getByTestId('t3');
+    expect(linkElement.getAttribute('value')).toBe('a3');
+
+    fireEvent.change(linkElement, { target: { value: 'ttttt3' } });
+    expect(linkElement.getAttribute('value')).toBe('ttttt3');
+  });
+
+})
